2f499136467c80c0ddc21d6168c8e2ddcba8b161
Youness El Andaloussi <youness@elandaloussi.net>
2024-06-21 01:24:14 +0100

    jquery is now local

--- templates/list_members.php
+++ templates/list_members.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <title>List Family Tree Members</title>
-    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+    <script src="res/jquery-3.6.0.min.js"></script>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="res/style.css"> 
 
