3e8225f8fc28e748c79e795f13f249676dd6fb43
Youness El Andaloussi <youness@elandaloussi.net>
2024-07-03 08:08:46 +0100

    viewport on edit_member

--- templates/edit_member.php
+++ templates/edit_member.php
@@ -5,6 +5,9 @@
     <title>Edition Membre</title>
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
     <!--<link rel="stylesheet" href="res/style.css?Version=1">-->
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
     <title>Genie: Membres Famille</title>
     <!-- Bootstrap CSS -->
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
