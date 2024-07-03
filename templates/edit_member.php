c88dc94cf41b2ff5122f23027e8ea375076f24a1
Youness El Andaloussi <youness@elandaloussi.net>
2024-07-03 08:11:55 +0100

    add jquery popper to fix menu

--- templates/edit_member.php
+++ templates/edit_member.php
@@ -5,6 +5,10 @@
     <title>Edition Membre</title>
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
     <!--<link rel="stylesheet" href="res/style.css?Version=1">-->
+    <!-- <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script> -->
+
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@1.16.1/dist/umd/popper.min.js"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
