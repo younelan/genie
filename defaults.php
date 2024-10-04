7df55d0e3372869bae6b1ea6269f3ba9816a2a4f
Youness El Andaloussi <youness@elandaloussi.net>
2024-10-04 11:05:56 +0100

    Translations for list_members

---
+++ defaults.php
@@ -0,0 +1,141 @@
+<?php
+$config = [];
+$vars = [
+   "charset" => "utf-8",
+   "favicon" => "favicon.png",
+   "themepath" => "themes/bootstrap",
+   "sitepath" => trim(dirname($_SERVER['PHP_SELF']), "/"),
+   "pagebefore" => "",
+   "pageafter" => "",
+   "contentbefore" => "",
+   "contentafter" => "",
+   "pagestyle" => "",
+   "logoimg" => "images/ControlTower.gif",
+   "favicon" => 'images/ControlTowerIcon.png',
+   "title" => "Control Tower",
+   "footerfg" => "#5d6d8a",
+   "footerbg" => "#2a6370",
+   "menubg" => "#061e38",
+   "menufg" => "white",
+   "pagebg" => "#396d89",
+   "pagefg" => "black",
+   "contentbefore" => "",
+   "contentafter" => "",
+   "footer" => "(c) 2024 Periscope Server Control",
+   "sitename" => "Periscope Server Control",
+   "pagestyle" => ""
+];
+
+//print "<pre>";
+$sitepath = trim(dirname($_SERVER['PHP_SELF']), "/");
+
+if (!$sitepath) {
+   $sitepath = '.';
+} else {
+   $sitepath = "$sitepath";
+}
+$vars['sitepath'] = $sitepath;
+
+//navigationmenu
+$translations["fr"] = [
+   "Family Members"=>"Membres de la Famille",
+   "Search By Name"=>"Chercher Par Nom",
+   "Pages"=>"Pages",
+   "Recent Activity"=>"Activités Récentes",
+   "Men"=>"Hommes",
+   "Women"=>"Femmes",
+   "By First Name"=>"Par Prénom",
+   "By Last Name"=>"Par Nom",
+   "By Gender"=>"Par Sexe",
+   "Relations"=> "Relations",
+   "Events"=>"Evènements",
+   "Other"=>"Autres",
+   "Statistics"=>"Statistiques",
+   "Edit Relationship"=>"Editer Relation",
+   "Add Relationship"=>"Add Relation",
+   "Delete Relationship"=>"Effacer Relation",
+   "Trees"=>"Arbres",
+   "Visualize"=>"Visualiser",
+   "New Member"=>"Nouveau Membre",
+   "View Tree" => "Voir Arbre",
+   "Ok" > "Ok",
+   "Fail" => "Echoué",
+   "Collapse" => "Réduire",
+   "Expand" => "Montrer",
+   "Failed Login" => "Echec Connection",
+   "Invalid CSRF token" => "Echec Verif Code CSRF",
+   "info" => "Info",
+   "Time" => "Heure",
+   "Amount" => "Qté",
+   "Sender" => "Expediteur",
+   "Recipient" => "Destinataire",
+   "Network" => "Réseau",
+   "Network Hosts" => "Machines sur le Réseau",
+   "Load" => "Proc",
+   "Uptime" => "Dispo",
+   "Device Details" => "Détails Machine",
+   "Ops Wellness" => "Santé Opérationnelle",
+   "Queue is Empty, Back to " => "Queue Vide, Retour Vers ",
+   "Mac Address" => "Adresse Mac",
+   "Host Name" => "Nom Machine",
+   "Details" => "Détails",
+   "Vendor" => "Marque",
+   "Security" => "Sécurité",
+   "Logon" => "Connecter",
+   "Connect" => "Connecter",
+   "Logoff" => "Déconnecter",
+   "Delete" => "Effacer",
+   "All" => "Tout",
+   "None" => "Aucun",
+   "Select" => "Selection",
+   "Drag a field to Move" => "Glisser un champ pour Réorganiser",
+   "Click Hide/Show to Hide/Show Fields" => "Montrer/Cacher affiche/cache Champ",
+   "Click a field to Sort that Column" => "Clic sur entête trie la Colonne",
+   "Labels" => "L&eacute;gende",
+   "Mail Queue Contents" => "Queue Postfix",
+   'Password' => 'Mot de passe',
+   'Login' => 'Identifiant',
+   'Current Password' => 'Mot de passe Actuel',
+   'New Password' => 'New Password',
+   'Confirm Password' => 'Confirmer Mot de Passe',
+   'One Digit' => 'Password must contain at leat one Digit',
+   'One Uppercase' => 'Password must contain at leat one Uppercase',
+   'One Lowercase' => 'Password must contain at leat one Lowercase',
+   'One Special' => 'Password must contain at leat one Special Character',
+   'Min Length' => 'Password must be at least 8 characters long',
+   'Edit Password' => 'Changer Mot de passe',
+   'Change Password' => 'Changer Mot de Passe',
+   'Password Changed' => 'Mot de passe Changé',
+   'Password Change Failed' => 'Changement de Mot de passe échoué',
+   'Current Password Incorrect' => 'Mot de passe Actuel Incorrect',
+   'Please Login' => 'Veuillez Connecter',
+   'Please Enter Password' => "Entrer Mot de Passe",
+   "Update Password" => "Mettre à Jour Mot de passe",
+   'Login Required' => "Mot de Passe Requis",
+];
+
+$keys = array_keys($translations['fr']);
+
+$translations['en'] = array_combine($keys, $keys);
+
+$config['translations'] = $translations;
+$config['paths'] = [
+   'frontend' => dirname(__DIR__) . "/frontend",
+   'base' => dirname(__DIR__),
+   'data' => __DIR__ . "/data",
+   'backend' => __DIR__,
+   'plugins' => dirname(__FILE__) . "/plugins",
+];
+$config_files = ["default_config.json", "data/config.json"];
+foreach ($config_files as $fname) {
+   if (file_exists(__DIR__ . "/$fname")) {
+      $cfg = file_get_contents(__DIR__ . "/$fname");
+      $json_config = json_decode($cfg, true);
+      if ($json_config) {
+         $config = array_replace_recursive($config, $json_config);
+         // foreach($json_config as $key=>$value) {
+         //     $config[$key]=$value;
+         // }
+      }
+   }
+}
