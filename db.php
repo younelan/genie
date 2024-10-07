75e283632786bf3e0f0d76efd33b2faed13c7593
Youness El Andaloussi <youness@elandaloussi.net>
2024-10-07 20:24:35 +0100

    index updated to send connection as part of a config array to allow passing params

--- db.php
+++ db.php
@@ -5,8 +5,9 @@ $dbUser = $config['db']['user'] ?? 'root';
 $dbPass = $config['db']['pass']??'';
 
 try {
-    $db = new PDO("mysql:host=$dbHost;dbname=$dbName", $dbUser, $dbPass);
-    $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    $connection = new PDO("mysql:host=$dbHost;dbname=$dbName", $dbUser, $dbPass);
+    $connection->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    $config['connection'] = $connection;
 } catch (PDOException $e) {
     die("Database connection failed: " . $e->getMessage());
 }
