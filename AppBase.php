ac11a9627413ca7e9abeac53863c6357cc013771
Youness El Andaloussi <youness@elandaloussi.net>
2024-10-09 17:50:38 +0100

    Add render_string to App Base

--- AppBase.php
+++ AppBase.php
@@ -1,7 +1,19 @@
 <?php 
 class AppBase
 {
-    
+
+    function render_file($fname, $blocks)
+    {
+
+        $contents = @file_get_contents($fname);
+        return $this->render_string($contents,$blocks);
+    }
+    function render_string($contents, $blocks)
+    {
+        $helper = new \Opensitez\Simplicity\SimpleTemplate();
+        return $helper->render($contents, $blocks);
+    }
+
 }
 class AppModel extends AppBase {
 
