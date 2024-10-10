7d92315a685d459af2e8a06da9bc4cf0b8f62249
Youness El Andaloussi <youness@elandaloussi.net>
2024-10-10 10:01:36 +0100

    fixed twig call

--- AppBase.php
+++ AppBase.php
@@ -6,12 +6,25 @@ class AppBase
     {
 
         $contents = @file_get_contents($fname);
-        return $this->render_string($contents,$blocks);
+        return $this->renderTwigTemplate($contents,$blocks);
+        //return $this->render_string($contents,$blocks);
     }
     function render_string($contents, $blocks)
     {
         $helper = new \Opensitez\Simplicity\SimpleTemplate();
         return $helper->render($contents, $blocks);
+    }
+    function renderTwigTemplate(string $templateContent, array $variables,$template_name="content") {
+        if(!is_array($templateContent)) {
+            $templateContent = [ "content"=>$templateContent];
+        }
+
+        $loader = new \Twig\Loader\ArrayLoader($templateContent);
+        $twig = new \Twig\Environment($loader);
+
+        $renderedContent = $twig->render($template_name, $variables);
+    
+        return $renderedContent;
     }
 
 }
